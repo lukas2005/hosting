67a24456fa62735920b6b1567cec263f92c122e7
lukas2005 <lukas2005.38@gmail.com>
2018-02-06 20:44:11 +0100

    Create countries.js

---
+++ countries.js
@@ -0,0 +1,16 @@
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
+
+let json;
+
+httpGetAsync("", function(jsonString) {
+	json = JSON.parse(jsonString);
+});
